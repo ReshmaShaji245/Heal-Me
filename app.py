4f9d08e57612614e9bedc27dee0e2d268b77948d
ReshmaShaji245 <62711806+ReshmaShaji245@users.noreply.github.com>
2021-08-29 00:05:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,7 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 import requests
 
-
-
 app = Flask(__name__)
-
-
-# Check for environment variable
-#if not os.getenv("DATABASE_URL"):
-#    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-
 
 
 @app.route("/")
