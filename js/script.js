dbeb48765349869a88351f42c5acf5a2a0cda9c5
Rehannah <rehannah12345@gmail.com>
2021-08-28 20:30:06 -0400

    changes made

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//This function will paste the questions related to the body part selected in the dropdown
+function Questions_knee() {
+    var questions = document.getElementById("questions");
+    questions.innerHTML = ""; //empty the space just in case something else was there
+
+    let pastequestions = `
+   `;
+    questions.innerHTML = pastequestions;
+
+}
